a77500b69269b006820e7fe26e7a9744051e50f7
yooud <khijnyak.dima@gmail.com>
2025-05-23 17:03:20 +0000

    Implement error handling in API service methods to return error responses

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,9 +1,17 @@
 import api from "@/api";
+import type { Error } from "@/types/api";
 import type { 
     LoginResponse
  } from "@/types/auth";
+import type {  } from 'axios';
+import { AxiosError} from "axios";
 
-export const login = async (): Promise<LoginResponse> => {
-  const response = await api.post("/auth");
-  return response.data;
+export const login = async (): Promise<LoginResponse | Error> => {
+  try {
+    const response = await api.post("/auth");
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 };
--- frontend/src/services/habitService.ts
+++ frontend/src/services/habitService.ts
@@ -1,4 +1,5 @@
 import api from "@/api";
+import type { Error } from "@/types/api";
 import type { 
   Habit,
   CreateHabitRequest,
@@ -9,56 +10,112 @@ import type {
   CompleteHabitResponse,
   HabitCompletionResponse
 } from "@/types/habit";
+import { AxiosError} from "axios";
 
-export const getHabits = async (): Promise<Habit[]> => {
-  const response = await api.get('/habits');
-  return response.data;
+export const getHabits = async (): Promise<Habit[] | Error> => {
+  try {
+    const response = await api.get('/habits');
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 };
 
-export const getHabit = async (id: string): Promise<Habit> => {
-  const response = await api.get(`/habits/${id}`);
-  return response.data;
+export const getHabit = async (id: string): Promise<Habit | Error> => {
+  try {
+    const response = await api.get(`/habits/${id}`);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
 
-export const createHabit = async (data: CreateHabitRequest): Promise<Habit> => {
-  const response = await api.post('/habits', data);
-  return response.data;
+export const createHabit = async (data: CreateHabitRequest): Promise<Habit | Error> => {
+  try {
+    const response = await api.post('/habits', data);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
 
-export const updateHabit = async (id: string, data: Partial<CreateHabitRequest>): Promise<Habit> => {
-  const response = await api.patch(`/habits/${id}`, data);
-  return response.data;
+export const updateHabit = async (id: string, data: Partial<CreateHabitRequest>): Promise<Habit | Error> => { 
+  try {
+    const response = await api.patch(`/habits/${id}`, data);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
 
-export const removeHabit = async (id: string): Promise<void> => {
-  await api.delete(`/habits/${id}`);
+export const removeHabit = async (id: string): Promise<void | Error> => {
+  try {
+    await api.delete(`/habits/${id}`);
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
 
-export const assignHabit = async (id: string, data: AssignHabitRequest): Promise<AssignHabitResponse> => {
-  const response = await api.post(`/habits/${id}/assign`, data);
-  return response.data;
+export const assignHabit = async (id: string, data: AssignHabitRequest): Promise<AssignHabitResponse | Error> => {
+    try {
+    const response = await api.post(`/habits/${id}/assign`, data);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
 
 export const getMyHabits = async (): Promise<HabitResponse> => {
-  const response = await api.get('/habits/assigned/me');
-  return response.data;
+  try {
+    const response = await api.get('/habits/assigned/me');
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
 
-export const updateAssignedHabit = async (id: string, data: UpdateAssignedHabitRequest): Promise<void> => {
-  const response = await api.patch(`/habits/assigned/${id}`, { params: data });
-  return response.data;
+export const updateAssignedHabit = async (id: string, data: UpdateAssignedHabitRequest): Promise<void | Error> => {
+  try {
+    const response = await api.patch(`/habits/assigned/${id}`, { params: data });
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
 
-export const removeAssignedHabit = async (id: string): Promise<void> => {
-  await api.patch(`/habits/assigned/${id}`);
+export const removeAssignedHabit = async (id: string): Promise<void | Error> => {
+  try {
+    await api.patch(`/habits/assigned/${id}`);
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
 
-export const completeHabit = async (id: string): Promise<CompleteHabitResponse> => {
-  const response = await api.post(`/habits/assigned/${id}`);
-  return response.data;
+export const completeHabit = async (id: string): Promise<CompleteHabitResponse | Error> => {
+  try {
+    const response = await api.post(`/habits/assigned/${id}`);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
 
-export const getHabitCompletions = async (id: string): Promise<HabitCompletionResponse> => {
-  const response = await api.post(`/habits/${id}/completions`);
-  return response.data;
+export const getHabitCompletions = async (id: string): Promise<HabitCompletionResponse | Error> => {
+  try {
+    const response = await api.post(`/habits/${id}/completions`);
+    return response.data;
+  } catch (error) {
+    if (error instanceof AxiosError)
+      return error.response?.data;
+  }
 }
