80ba943e3ce75d38b1cff186a7a19520b1a9b403
yooud <khijnyak.dima@gmail.com>
2025-05-23 17:13:04 +0000

    Add PrimeVue and @primeuix/themes integration in main.ts, update package.json dependencies

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,49 @@
 import { createApp } from 'vue'
 import '@/assets/styles.scss'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
+import { createPinia } from 'pinia';
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
+import { ConfirmationService, Ripple, ToastService } from 'primevue';
 
-createApp(App).mount('#app')
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{indigo.50}',
+            100: '{indigo.100}',
+            200: '{indigo.200}',
+            300: '{indigo.300}',
+            400: '{indigo.400}',
+            500: '{indigo.500}',
+            600: '{indigo.600}',
+            700: '{indigo.700}',
+            800: '{indigo.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
+        }
+    }
+});
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(PrimeVue, {
+    theme: {
+        preset: MyPreset,
+        options: {
+            cssLayer: {
+                name: 'primevue',
+                order: 'tailwind-base, primevue, tailwind-utilities'
+            },
+            darkModeSelector: '.app-dark'
+        }
+    },
+    ripple: true
+});
+app.directive('ripple', Ripple);
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.mount('#app')
